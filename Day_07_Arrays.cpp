68a6fcda1ff44c403127c0357cc9f8da50e99a12
mohitgora55 <51796016+mohitgora55@users.noreply.github.com>
2020-06-18 17:27:54 +0530

    Create Day_07_Arrays.cpp

---
+++ Day_07_Arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+  int a;
+  cin>>a;
+  int arr[a];
+  for(int i=0;i<a;i++)
+  {
+    cin>>arr[i];
+  }
+  for(int i=a-1;i>=0;i--)
+  {
+    cout<<arr[i]<<" ";
+  }
+  return 0;
+}
